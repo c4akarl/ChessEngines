c63162293cec64ac666caae9937cfe05e292b5af
Karl Schreiner <c4akarl@gmail.com>
2020-09-17 13:42:23 +0200

    version 2.1

--- app/src/main/java/ccc/chess/engines/MyEngineProvider.java
+++ app/src/main/java/ccc/chess/engines/MyEngineProvider.java
@@ -1,5 +1,8 @@
 package ccc.chess.engines;
 
+import android.app.Application;
+import android.content.Context;
+
 import com.kalab.chess.enginesupport.ChessEngineProvider;
 
 public class MyEngineProvider extends ChessEngineProvider {
